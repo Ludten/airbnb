51f7650233b3ffaab31fb354d6eef6b3bdda7ab7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-03 14:46:46 +0100

    print __str__

--- console.py
+++ console.py
@@ -46,21 +46,82 @@ class HBNBCommand(cmd.Cmd):
         """ show [class] [id]
         Print instance of the cls with the passed id
         """
-        if arg == () or arg is None:
+        if arg == '' or arg is None:
             print("** class name missing **")
         else:
-            print(arg)
-            # cls_id = "{:s}.{:s}".format(cls, id)
-            # allobjects = storage.all()
-            # if cls == 'BaseModel':
-            #     if cls_id in list(allobjects.keys()):
-            #         print(str(allobjects[cls_id]))
-            #     else:
-            #         print("** no instance found **")
-            # else:
-            #     print("** class doesn't exist **")
+            if '"' in arg:
+                narg = arg.split('"')
+                for i in range(len(narg)):
+                    narg[i] = narg[i].strip()
+                larg = [ele for ele in narg if ele != '']
+            else:
+                larg = arg.split(' ')
+            try:
+                cls_id = "{:s}.{:s}".format(larg[0], larg[1])
+                allobjects = storage.all()
+                if larg[0] == 'BaseModel':
+                    if cls_id in list(allobjects.keys()):
+                        newmodel = BaseModel(allobjects[cls_id])
+                        print(newmodel.__str__())
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** class doesn't exist **")
+            except IndexError:
+                print("** instance id missing **")
 
-    # def do_destroy(self,)
+    def do_destroy(self, arg):
+        """destroy [class] [id]
+        delete an instance of the cls with the passed id
+        """
+        if arg == '' or arg is None:
+            print("** class name missing **")
+        else:
+            if '"' in arg:
+                narg = arg.split('"')
+                for i in range(len(narg)):
+                    narg[i] = narg[i].strip()
+                larg = [ele for ele in narg if ele != '']
+            else:
+                larg = arg.split(' ')
+            try:
+                cls_id = "{:s}.{:s}".format(larg[0], larg[1])
+                allobjects = storage.all()
+                if larg[0] == 'BaseModel':
+                    if cls_id in list(allobjects.keys()):
+                        del allobjects[cls_id]
+                        storage.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** class doesn't exist **")
+            except IndexError:
+                print("** instance id missing **")
+
+    def do_all(self, arg):
+        """ all [model]?
+        Prints all string representation of all instances based
+        or not on the class name.
+        """
+        allobjects = storage.all()
+        if arg == '':
+            for k, v in allobjects.items():
+                if 'BaseModel' in k:
+                    newmodel = BaseModel(v)
+                    print(newmodel.__str__())
+        else:
+            if arg == 'BaseModel':
+                for k, v in allobjects.items():
+                    if 'BaseModel' in k:
+                        newmodel = BaseModel(v)
+                        print(newmodel.__str__())
+            else:
+                print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """update <class name> <id> <attribute name> "<attribute value>"
+        Update an attribute of the passes class instance
+        """
 
 
 if __name__ == '__main__':
